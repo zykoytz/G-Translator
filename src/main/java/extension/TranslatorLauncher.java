15802f5541ae8694040b344b3fd0a63eab49b439
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2022-05-03 04:49:32 +0200

    update to g-earth 1.5.2 + themed extension

--- src/main/java/extension/TranslatorLauncher.java
+++ src/main/java/extension/TranslatorLauncher.java
@@ -1,35 +1,22 @@
 package extension;
 
-import gearth.Main;
-import gearth.extensions.ExtensionForm;
-import gearth.extensions.ExtensionFormCreator;
-import gearth.ui.GEarthController;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
+import gearth.extensions.ThemedExtensionFormCreator;
 
-public class TranslatorLauncher extends ExtensionFormCreator {
+import java.net.URL;
 
-    public ExtensionForm createForm(Stage primaryStage) throws Exception {
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("translator.fxml"));
-        Parent root = loader.load();
-
-        primaryStage.setTitle("G-Translator");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.getScene().getStylesheets().add(GEarthController.class.getResource("/gearth/themes/G-Earth/styling.css").toExternalForm());
-        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/gearth/themes/G-Earth/logoSmall.png")));
-
-        primaryStage.setResizable(false);
-
-        return loader.getController();
-
-    }
+public class TranslatorLauncher extends ThemedExtensionFormCreator {
 
     public static void main(String[] args) {
         runExtensionForm(args, TranslatorLauncher.class);
     }
 
+    @Override
+    protected String getTitle() {
+        return "G-Translator";
+    }
+
+    @Override
+    protected URL getFormResource() {
+        return getClass().getResource("translator.fxml");
+    }
 }
